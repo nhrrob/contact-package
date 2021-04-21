49a4b8ec7d672b6b3d6d0dd5a5f923949fcb2d01
Nazmul Hasan Robin <robin.sust08@gmail.com>
2021-04-21 22:06:06 +0600

    vendor files can be published

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -1,26 +1,30 @@
 <?php
+
 namespace Nhrrob\Contact;
 
 use Illuminate\Support\ServiceProvider;
 
-class ContactServiceProvider extends ServiceProvider{
+class ContactServiceProvider extends ServiceProvider
+{
 
-    public function boot(){
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views','contact');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations/');
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/views', 'contact');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations/');
 
         $this->mergeConfigFrom(
-            __DIR__.'/config/contact.php', 'contact'
+            __DIR__ . '/config/contact.php',
+            'contact'
         );
 
         $this->publishes([
-            __DIR__.'/config/contact.php' => config_path('contact.php'),
-        ]);    
-
+            __DIR__ . '/config/contact.php' => config_path('contact.php'),
+            __DIR__ . '/views' => resource_path('views/vendor/contact'),
+        ]);
     }
 
-    public function register(){
-        
+    public function register()
+    {
     }
 }
